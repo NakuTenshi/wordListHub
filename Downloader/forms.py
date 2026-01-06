dda8708632b6ae7b829e3c3d4b6e12afa06acd8b
naku.tenshi <naku.tenshii@gmail.com>
2026-01-06 18:36:42 +0330

    remove Downloader from github

--- Downloader/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import upload_wordlist
-
-class uploadwordlistform(ModelForm):
-    class Meta:
-        model = upload_wordlist
-        fields = ["path" , "file"]
--- Downloader/templatetags/custom_filter.py
+++
@@ -1,8 +0,0 @@
-from django.template import Library
-
-register = Library()
-
-@register.filter
-def get_lastvalue(value):
-    if value:
-        return str(value).split("/")[-1]
